71bfeebc2f5b527bb0126bed6a00766828bcc464
Mika Rinne <mika.rinne@oracle.com>
2016-10-16 10:58:27 +0300

    test connect fix

--- dbconfig.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-  user          : process.env.NODE_ORACLEDB_USER || "mika",
-  password      : process.env.NODE_ORACLEDB_PASSWORD || "pass",
-  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "140.86.0.141:1521/PDB1.gse00000504.oraclecloud.internal",
-};
